ee8e00e3ee39dbaf3bd65d54077125b100d37a97
agus <aalma@mail.austral.edu.ar>
2024-04-17 23:52:54 -0300

    redo signup.js and added jwt for token

--- src/main/backend/controller/signup.js
+++ src/main/backend/controller/signup.js
@@ -1,45 +1,32 @@
+const jwt = require('jsonwebtoken');
 const Signup  = require('../model/signup');
 const {USER} = require("../config/dbConfig");
 const User = require('../model/user')
 
-module.exports = {
+const expirationTime = '1h';
 
-    create: async (req, res) => {
-        if (req.body.name && req.body.email && req.body.password) {
-            const {name, email, password} = req.body;
-            const user = await User.create( )
+exports.signUp = async (req, res) => {
+    const {name, email, password} = req.body;
+    let existingUser = await Signup.findOne({
+        where: {email}
+    });
+    if (existingUser) {
+        return res.status(400).json({message: 'User already exists'});
+    }
+    const user = User.create({
+        name,
+        email,
+        password
+    });
+    //hacer el token y enviar el token
+    try {
+        const token = jwt.sign({id: user.userId}, USER, {
+            expiresIn: expirationTime
+        });
+        res.status(201).json({message: 'User created successfully', token : token}) //atajo este json en el front
 
-            console.log(user)
-
-            await Signup.create({
-                name,
-                email,
-                password,
-                userId: user.id
-            });
-
-            res.cookie('name', name, { secure: true });
-        }
-        else {
-            res.send('Not added to the database!');
-        }
-    },
-
-    signup: async (req, res) => {
-        if(req.body.name && req.body.email && req.body.password) {
-            const {name, email, password} = req.body;
-
-            let user = await Signup.findOne({
-                where: {name, email, password}
-            });
-
-            if (user) {
-                req.session.user = user;
-                req.session.authorized = true;
-            }
-            else {
-                res.render('signup');
-            }
-        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server error'});
     }
 }
